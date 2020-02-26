3a93e59e61a4fc71ece0f49a57b4a8a57a0c95c0
Oliver Broadrick <obroadrick@gwupyterhub.seas.gwu.edu>
2020-02-26 12:16:53 -0500

    created database tables

--- database/db.sql
+++ database/db.sql
@@ -1,1 +1,20 @@
-woohoo
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS account CASCADE;
+CREATE TABLE account (
+  username      varchar(20) not null, 
+  password      varchar(100) not null,
+  primary key (username)
+);
+
+DROP TABLE IF EXISTS high_score CASCADE;
+CREATE TABLE high_score (
+  high_score      int(8) not null,
+  username      varchar(20) not null,
+  datetime      datetime, 
+  foreign key (username) references account(username)
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
