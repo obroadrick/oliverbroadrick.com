7698efac36588cf79109b4011db019130f2b623c
Oliver Broadrick <odbroadrick@gmail.com>
2022-07-12 18:26:47 -0400

    fish pics git push

---
+++ html/gen.py
@@ -0,0 +1,9 @@
+for i in range(1,29):
+    print("<div class=\"gallery\">\n\
+  <a target=\"_blank\" href=\"troutpics/%03d.jpg\">\n\
+    <img src=\"troutpics/%03d.jpg\" alt=\"a trout pic\" width=\"200\" height=\"140\">\n\
+  </a>\n\
+  <div class=\"desc\">Trout pic!!</div>\n\
+</div>\n\n" % (i, i))
+
+   
---
+++ imgs/rename.py
@@ -0,0 +1,19 @@
+a=1
+for i in *.jpg; do
+  new=$(printf "%04d.jpg" "$a") #04 pad to length of 4
+  mv -i -- "$i" "$new"
+  let a=a+1
+done
+
+
+
+# assign directory
+directory = 'trout-pics'
+
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(f)
